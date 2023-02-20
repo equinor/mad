f6a81d30249add9f92b48ea781af20b5a8ee4d59
Haakon Svane <36899138+HaakonSvane@users.noreply.github.com>
2023-02-20 20:27:19 +0100

    feat: skia draw workspace (#15)
    - Initialize skia draw workspace
    - Add draw tab screen to chronicles 
    - Add basic (non-working) canvas to react native skia draw

---
+++ apps/chronicles/screens/DrawScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import {Canvas} from "@equinor/react-native-skia-draw/dist/Canvas";
+
+
+export const DrawScreen = () => {
+    return (
+     <Canvas />
+    );
+}
