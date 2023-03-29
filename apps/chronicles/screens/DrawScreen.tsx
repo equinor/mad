51a7b79ca5620a5036bb638261bd36824ce445ec
HaakonSvane <haakonsvane@gmail.com>
2023-03-29 13:33:20 +0200

    Add expo image picker and write draw screen in chronicles

--- apps/chronicles/screens/DrawScreen.tsx
+++ apps/chronicles/screens/DrawScreen.tsx
@@ -1,9 +1,78 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { DFWCanvas } from "@equinor/react-native-skia-draw/dist/premades";
-
+import { Button, Typography } from "@equinor/mad-components";
+import * as ImagePicker from "expo-image-picker";
+import { useRef, useState } from "react";
+import { SignaturePadHandle } from "@equinor/react-native-skia-draw/dist/types";
 
 export const DrawScreen = () => {
+    const canvasHandle = useRef<SignaturePadHandle>(null);
+    const [originalImage, setOriginalImage] = useState<string>();
+    const [modifiedImage, setModifiedImage] = useState<string>();
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: false,
+            quality: 1,
+        });
+        if (!result.canceled) {
+            setOriginalImage(result.assets[0].uri);
+        }
+    };
+
+    const saveSnapshot = () => {
+        if (!!canvasHandle?.current?.makeImageSnapshot) {
+            const image = canvasHandle.current.makeImageSnapshot();
+            setModifiedImage(image?.uri);
+        }
+    };
+
     return (
-        <DFWCanvas />
+        <>
+            <DFWCanvas ref={canvasHandle} style={{ flex: 3 }} markupImageUri={originalImage} />
+            <View style={styles.inOutContainer}>
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Typography variant="h6" style={{ margin: 7 }}>Original image:</Typography>
+                    <View style={styles.imageContainer}>
+                        <Image source={{ uri: originalImage }} style={{ height: "100%" }} />
+                    </View>
+                    <Button
+                        onPress={pickImage}
+                        style={{ margin: 10 }}>
+                        + Add photo
+                    </Button>
+
+                </View>
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Typography variant="h6" style={{ margin: 7 }}>Modified image:</Typography>
+                    <View style={styles.imageContainer}>
+                        <Image source={{ uri: modifiedImage }} style={{ height: "100%" }} />
+                    </View>
+                    <Button
+                        style={{ margin: 10 }}
+                        onPress={saveSnapshot}
+                    >
+                        Save snapshot
+                    </Button>
+                </View>
+            </View>
+        </>
     );
 }
+
+
+const styles = StyleSheet.create({
+    inOutContainer: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    imageContainer: {
+        alignSelf: "center",
+        flex: 1,
+        width: "90%",
+        backgroundColor: "white",
+        borderRadius: 12,
+        overflow: "hidden"
+    },
+})
--- packages/skia-draw/src/premades/ImageMarkup/DFWCanvas.tsx
+++ packages/skia-draw/src/premades/ImageMarkup/DFWCanvas.tsx
@@ -1,21 +1,32 @@
-import { View, StyleSheet, SafeAreaView } from "react-native"
+import { View, StyleSheet, ViewProps } from "react-native"
 import { Canvas } from "../../Canvas"
 import { EDSControlPanel } from "./EDSControlPanel"
-import { useImperativeHandle, useRef } from "react";
+import { useImperativeHandle, useRef, useState } from "react";
 import { SkiaDrawHandle } from "../../types";
 import React from "react";
 import { SignaturePadHandle } from "../../types";
+import { useImage, Image as SKImage } from "@shopify/react-native-skia";
 
-export const DFWCanvas = React.forwardRef<SignaturePadHandle>((_, ref) => {
+export type DFWCanvas = {
+    markupImageUri?: string,
+} & ViewProps;
+
+export const DFWCanvas = React.forwardRef<SignaturePadHandle, DFWCanvas>((props, ref) => {
+    const [dimensions, setDimensions] = useState<{ width: number, height: number }>();
+
     const canvasRef = useRef<SkiaDrawHandle>(null);
-    useImperativeHandle(ref, () => ({ makeImageSnapshot: () => canvasRef.current?.makeImageSnapshot() || undefined }))
+    useImperativeHandle(ref, () => ({ makeImageSnapshot: () => canvasRef.current?.makeImageSnapshot({ x: 0, y: 100, width: dimensions?.width ?? 0, height: dimensions?.height ?? 0 }) || undefined }))
+    const image = useImage(props.markupImageUri);
+
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Canvas ref={canvasRef} />
+        <View {...props} onLayout={(event) => setDimensions({ width: event.nativeEvent.layout.width, height: event.nativeEvent.layout.height })}>
+            <Canvas ref={canvasRef}>
+                {image && dimensions && <SKImage image={image} fit="contain" x={0} y={0} width={dimensions.width} height={dimensions.height} />}
+            </Canvas>
             <View style={styles.overlay} pointerEvents="box-none">
                 <EDSControlPanel canvasRef={canvasRef} />
             </View>
-        </SafeAreaView>
+        </View>
     )
 });
 
