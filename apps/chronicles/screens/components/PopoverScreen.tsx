064958f5e1129fcdf6229b8ca4cf5959431951cc
Hathibelagal <127299081+hathibelagal-dev2@users.noreply.github.com>
2023-04-03 14:26:25 +0200

    feat: rounded arrow (#44)
    Arrow positioning works

--- apps/chronicles/screens/components/PopoverScreen.tsx
+++ apps/chronicles/screens/components/PopoverScreen.tsx
@@ -3,26 +3,27 @@ import { useRef, useState } from "react";
 import { ScrollView, View } from "react-native";
 
 export const PopoverScreen = () => {
-    const buttonRef = useRef<View>(null);
-    const [open, setOpen] = useState(false);
-    return (
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            contentContainerStyle={{ padding: 12 }}
-        >
-            <Typography style={{ marginBottom: 40 }}>
-                The popover component displays modally on top of your content and positions itself
-                automatically based on constraints around the anchor element.
-                Try scrolling the page to see this in action.
-            </Typography>
-            <Button ref={buttonRef} onPress={() => {
-                setOpen(!open);
-            }}>
-                Trigger popover
-            </Button>
-            <Popover open={open} anchorEl={buttonRef} onClose={() => setOpen(false)}>
-                <Typography>This is a popover</Typography>
-            </Popover>
-        </ScrollView>
-    );
+  const buttonRef = useRef<View>(null);
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState("top");
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      contentContainerStyle={{ padding: 12, display: "flex", alignItems: "center" }}
+    >
+      <Typography style={{ marginBottom: 40 }}>
+        The popover component displays modally on top of your content and positions itself
+        automatically based on constraints around the anchor element.
+        Try scrolling the page to see this in action.
+      </Typography>
+      <Button ref={buttonRef} onPress={() => {
+        setOpen(!open);
+      }} style={{ width: "50%" }}>
+        Trigger popover
+      </Button>
+      <Popover open={open} anchorEl={buttonRef} onClose={() => setOpen(false)} placement={placement}>
+        <Typography>This is a popover</Typography>
+      </Popover>
+    </ScrollView>
+  );
 };
--- packages/components/src/components/Popover/Popover.tsx
+++ packages/components/src/components/Popover/Popover.tsx
@@ -12,9 +12,20 @@ export type PopoverProps = {
   placement?: string;
 };
 
+type PopoverDimensions = {
+  width?: number;
+  height?: number;
+};
+
+const arrowContainerSize: number = 16;
+
 export const Popover = (props: PopoverProps & ViewProps) => {
 
   const arrowRef = useRef(null);
+  const popoverDimensions = useRef({
+    width: 0,
+    height: 0
+  } as PopoverDimensions)
   const {
     x,
     y,
@@ -23,7 +34,7 @@ export const Popover = (props: PopoverProps & ViewProps) => {
   } = useFloating({
     sameScrollView: false,
     middleware: [
-      offset(15),
+      offset(12),
       flip(),
       shift({ padding: 8 }),
       arrow({ element: arrowRef })
@@ -35,6 +46,20 @@ export const Popover = (props: PopoverProps & ViewProps) => {
     refs.setReference(props.anchorEl.current);
   }, [refs, props.anchorEl]);
 
+  let calculatedArrowX = (x as number) + (arrowX ?? 0);
+  let calculatedArrowY = (y as number) + (arrowY ?? 0);
+  if (props.placement?.startsWith("left")) {
+    calculatedArrowX += popoverDimensions.current.width ?? 0;
+  }
+  if (props.placement?.startsWith("top")) {
+    calculatedArrowY += popoverDimensions.current.height ?? 0;
+  }
+  if (props.placement?.startsWith("top") || props.placement?.startsWith("bottom")) {
+    calculatedArrowY -= arrowContainerSize / 2;
+  }
+  if (props.placement?.startsWith("left") || props.placement?.startsWith("right")) {
+    calculatedArrowX -= arrowContainerSize / 2;
+  }
   return (
     <Modal visible={props.open} transparent={true} presentationStyle="overFullScreen">
       <Pressable onPress={props.onClose} style={{ width: "100%", height: "100%", position: "absolute" }}>
@@ -42,10 +67,21 @@ export const Popover = (props: PopoverProps & ViewProps) => {
           style={{ position: "absolute", left: x ?? 0, top: y ?? 0, borderRadius: 12 }}
           elevation="overlay"
           ref={refs.setFloating}
+          onLayout={(e) => {
+            popoverDimensions.current.width = e.nativeEvent.layout.width;
+            popoverDimensions.current.height = e.nativeEvent.layout.height;
+          }}
         >
           <View style={[styles.innerContainer, props.style]} {...props}>{props.children}</View>
-          {/* <View ref={arrowRef} style={[styles.arrow, { left: arrowX ?? 0, top: arrowY ?? 0 }]}></View> */}
         </Paper>
+        <View ref={arrowRef} style={[styles.arrow, { left: calculatedArrowX, top: calculatedArrowY }]}>
+          <View style={{
+            width: arrowContainerSize / 1.444,
+            height: arrowContainerSize / 1.444,
+            transform: [{ rotate: "45deg" }],
+            backgroundColor: "white"
+          }}></View>
+        </View>
       </Pressable>
     </Modal>
   );
@@ -54,10 +90,11 @@ export const Popover = (props: PopoverProps & ViewProps) => {
 const styles = StyleSheet.create({
   arrow: {
     position: "absolute",
-    transform: [{ rotateZ: '45deg' }],
-    width: 25,
-    height: 25,
-    backgroundColor: "white",
+    width: arrowContainerSize,
+    height: arrowContainerSize,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   innerContainer: {
