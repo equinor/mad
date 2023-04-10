71badc1dad4afd6a4613a8b31eca80849e1f8487
HaakonSvane <haakonsvane@gmail.com>
2023-04-10 09:58:13 +0200

    Add dev script and tsup build to skia-draw

--- packages/skia-draw/tsup.config.ts
+++ packages/skia-draw/tsup.config.ts
@@ -2,18 +2,12 @@ import { spawnSync } from "child_process";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["./src/**/*.ts?(x)", "!./src/types.d.ts", "!./src/__tests__/*"],
+    entry: ["./src/**/*.ts?(x)", "!./src/__tests__/*"],
     splitting: false,
     clean: true,
     dts: false,
     format: "esm",
     tsconfig: "./tsconfig.json",
-    loader: {
-        ".otf": "copy",
-    },
-    esbuildOptions(options) {
-        options.assetNames = "assets/fonts/[name]"
-    },
     async onSuccess() {
         // eslint-disable-next-line no-console
         console.log("⚙️ Generating typescript declarations..");
