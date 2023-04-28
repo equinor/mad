ff8218a37bfd1d6275401c457ab8d4e4bbfa12e2
Ashraff Hathibelagal <127299081+hathibelagal-dev2@users.noreply.github.com>
2023-04-28 15:06:18 +0200

    label can have meta

--- packages/components/src/components/Label/Label.tsx
+++ packages/components/src/components/Label/Label.tsx
@@ -1,14 +1,20 @@
 import { Typography, TypographyProps } from "../Typography";
-
+import { View } from "react-native";
 export type LabelProps = {
     label?: string;
+    meta?: string;
 };
 
 export const Label = (props: LabelProps & TypographyProps) => {
-    const { label, ...other } = props;
-    return <Typography variant="label" {...other}>
-        {label}
-    </Typography>
+    const { label, meta, ...other } = props;
+    return <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Typography variant="label" {...other}>
+            {label}
+        </Typography>
+        <Typography variant="label" {...other}>
+            {meta}
+        </Typography>
+    </View>
 }
 
 Label.displayName = "Label";
