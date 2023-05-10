6228df0cf862d5178af1acba011d591b11e5c23a
HaakonSvane <haakonsvane@gmail.com>
2023-05-10 07:06:29 +0200

    fix merge issues

--- apps/chronicles/screens/DiscoverScreen.tsx
+++ apps/chronicles/screens/DiscoverScreen.tsx
@@ -2,7 +2,6 @@ import { ScrollView } from "react-native";
 import { DiscoverStackParamList } from "../types";
 
 import { Cell, Spacer } from "@equinor/mad-components";
-import { Cell, Spacer } from "@equinor/mad-components";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export default function DiscoverScreen({
@@ -16,7 +15,7 @@ export default function DiscoverScreen({
             <Cell.Group title="data display">
                 <Cell.Navigation
                     title="Popover"
-                    description="Contaner floating over some reference element with an arrow"
+                    description="Container floating over some reference element"
                     iconName="party-popper"
                     onPress={() => navigation.navigate("Popover")}
                 />
@@ -32,40 +31,27 @@ export default function DiscoverScreen({
 
             <Cell.Group title="inputs">
                 <Cell.Navigation
-            </Cell.Group>
-
-            <Spacer />
-
-            <Cell.Group title="inputs">
-                <Cell.Navigation
                     title="Button"
                     description="Buttons, buttons, and more buttons!"
-                    iconName="gesture-tap-button"
                     iconName="gesture-tap-button"
                     onPress={() => navigation.navigate("Button")}
                 />
                 <Cell.Navigation
-                <Cell.Navigation
                     title="TextField"
                     description="Enter and edit text"
-                    iconName="card-text-outline"
                     iconName="card-text-outline"
                     onPress={() => navigation.navigate("TextField")}
                 />
                 <Cell.Navigation
-                <Cell.Navigation
                     title="Input"
                     description="Fancier text input"
-                    iconName="form-textbox"
                     iconName="form-textbox"
                     onPress={() => navigation.navigate("Input")}
                 />
 
                 <Cell.Navigation
-                <Cell.Navigation
                     title="Search"
                     description="Search for content"
-                    iconName="magnify"
                     iconName="magnify"
                     onPress={() => navigation.navigate("Search")}
                 />
@@ -104,41 +90,6 @@ export default function DiscoverScreen({
                     onPress={() => navigation.navigate("NavigationCell")}
                 />
             </Cell.Group>
-            </Cell.Group>
-
-            <Spacer />
-
-            <Cell.Group title="surfaces">
-                <Cell.Navigation
-                    title="Paper"
-                    description="Multiple elevations and shadows"
-                    iconName="paper-roll-outline"
-                    onPress={() => navigation.navigate("Paper")}
-                />
-                <Cell.Navigation
-                    title="Accordion"
-                    description="Collapsable and expandable containers"
-                    iconName="arrow-collapse-vertical"
-                    onPress={() => navigation.navigate("Accordion")}
-                />
-            </Cell.Group>
-
-            <Spacer />
-
-            <Cell.Group title="cells">
-                <Cell.Navigation
-                    title="Cell"
-                    description="This screen is filled with them!"
-                    iconName="table-row"
-                    onPress={() => navigation.navigate("Cell")}
-                />
-                <Cell.Navigation
-                    title="Navigation Cell"
-                    description="Navigate to it by pressing one"
-                    iconName="sign-direction"
-                    onPress={() => navigation.navigate("NavigationCell")}
-                />
-            </Cell.Group>
-        </ScrollView >
+        </ScrollView>
     );
 }
