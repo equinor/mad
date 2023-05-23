58809f86957166d076233dd19ed178390a257e7b
HaakonSvane <haakonsvane@gmail.com>
2023-05-23 15:38:47 +0200

    Add iconName and adornment support for accordion

--- apps/chronicles/screens/components/AccordionScreen.tsx
+++ apps/chronicles/screens/components/AccordionScreen.tsx
@@ -34,11 +34,76 @@ export const AccordionScreen = () => {
                             </View>))}
                     </View>
                 </Accordion.Item>
-                <Accordion.Item title="Left-positioned chevron" chevronPosition="left">
-                    <Typography>But it defaults to right!</Typography>
+                <Accordion.Item title="Right-positioned chevron" chevronPosition="right">
+                    <Typography>But it defaults to left!</Typography>
                 </Accordion.Item>
                 <Accordion.Item title="This item is disabled!" disabled>
                     <Typography>But it defaults to right!</Typography>
+                </Accordion.Item>
+            </Accordion>
+
+            <Spacer amount="large" />
+
+            <View style={styles.readableContent}>
+                <Typography variant="h2">Adornments</Typography>
+                <Typography>
+                    You are free to add an adornment and / or an icon to the accordion items.
+                    Note that based on the position of the chevron, the layout will differ.
+                    See the following accordion for more info:
+                </Typography>
+            </View>
+            <Spacer />
+            <Accordion>
+                <Accordion.Item
+                    title="Left chevron"
+                    adornment={
+                        <View style={styles.adornment}>
+                            <Typography>Adornment</Typography>
+                        </View>
+                    }>
+                    <Typography>For left-positioned chevrons, the adornment is given plenty of space.</Typography>
+                </Accordion.Item>
+                <Accordion.Item
+                    title="Right chevron"
+                    chevronPosition="right"
+                    adornment={
+                        <View style={styles.adornment}>
+                            <Typography>Adornment</Typography>
+                        </View>
+                    }>
+                    <Typography>For right-positioned chevrons, the adornment is put in between it and the title.</Typography>
+                </Accordion.Item>
+                <Accordion.Item
+                    title="Right chevron with icon"
+                    chevronPosition="right"
+                    iconName="face-woman-shimmer-outline">
+                    <Typography>The icon is auto-positioned to make the accordion item line up with the grid-system.</Typography>
+                </Accordion.Item>
+                <Accordion.Item
+                    title="Left chevron with icon"
+                    iconName="face-man-shimmer-outline">
+                    <Typography>The chevron and the icon effectively switch places!</Typography>
+                </Accordion.Item>
+                <Accordion.Item
+                    title="Right chevron combo"
+                    chevronPosition="right"
+                    iconName="face-woman-profile"
+                    adornment={
+                        <View style={styles.adornment}>
+                            <Typography>Adornment</Typography>
+                        </View>
+                    }>
+                    <Typography>Things become more tight, but the layout still works.</Typography>
+                </Accordion.Item>
+                <Accordion.Item
+                    title="Left chevron combo"
+                    iconName="face-man-profile"
+                    adornment={
+                        <View style={styles.adornment}>
+                            <Typography>Adornment</Typography>
+                        </View>
+                    }>
+                    <Typography>Consider the available space for smaller devices as well. Use with caution!</Typography>
                 </Accordion.Item>
             </Accordion>
         </ScrollView >
@@ -56,5 +121,15 @@ const themeStyles = EDSStyleSheet.create(theme => ({
         flexDirection: "row",
         height: 30,
         gap: 4,
+    },
+    adornment: {
+        flex: 1,
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center",
+        borderStyle: "dashed",
+        borderColor: theme.colors.interactive.primary,
+        borderRadius: theme.geometry.border.elementBorderRadius,
+        paddingHorizontal: theme.spacing.container.paddingHorizontal
     },
 }));
