a10209b1cf2d98dc7109376e7f436a455273f507
HaakonSvane <haakonsvane@gmail.com>
2023-06-09 14:00:04 +0200

    Add adornment prop for Cell Group compoennt

--- packages/components/src/components/Cell/CellGroup.tsx
+++ packages/components/src/components/Cell/CellGroup.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { createContext } from "react";
 import { Typography } from "../Typography";
 import { EDSStyleSheet } from "../../styling";
 import { useStyles } from "../../hooks/useStyles";
 import { useValidChildrenIndexes } from "../../hooks/useValidChildrenIndexes";
+import { View } from "react-native";
 
 export type CellGroupContextType = {
     isFirstCell: boolean;
@@ -16,24 +17,31 @@ export const CellGroupContext = createContext<CellGroupContextType>({
 
 export type CellGroupProps = {
     title?: string;
+    adornment?: ReactNode;
 }
 
 export const CellGroup = ({
     title,
+    adornment,
     children,
 }: React.PropsWithChildren<CellGroupProps>) => {
     const styles = useStyles(themeStyles);
     const validChildrenIndexes = useValidChildrenIndexes(children);
     return (
         <>
-            {title &&
-                <Typography
-                    group="cell"
-                    variant="groupTitle"
-                    color="textTertiary"
-                    style={styles.title}>
-                    {title}
-                </Typography>}
+            <View style={styles.titleContainer}>
+                {title &&
+                    <Typography
+                        group="cell"
+                        variant="groupTitle"
+                        color="textTertiary">
+                        {title}
+                    </Typography>}
+                <View>
+                    {adornment}
+                </View>
+
+            </View>
             {React.Children.map(children, (child, index) => (
                 <CellGroupContext.Provider value={{
                     isFirstCell: index === validChildrenIndexes.at(0),
@@ -47,8 +55,12 @@ export const CellGroup = ({
 };
 
 const themeStyles = EDSStyleSheet.create(theme => ({
-    title: {
+    titleContainer: {
         paddingHorizontal: theme.spacing.container.paddingHorizontal,
         paddingBottom: theme.spacing.cell.group.titleBottomPadding,
+        flexDirection: "row",
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "flex-end"
     }
 }));
