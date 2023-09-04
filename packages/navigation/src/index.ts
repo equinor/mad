5a05d71930eb2836aa8a039d9674e34d557facca
HaakonSvane <haakonsvane@gmail.com>
2023-09-04 09:25:39 +0200

    move mad components to dep. reinstall repo. export environmentprovider through navigation

--- packages/navigation/src/index.ts
+++ packages/navigation/src/index.ts
@@ -1,2 +1,3 @@
-export * from './bottom-tab'
-export * from './native-stack'
+export * from "./bottom-tab";
+export * from "./native-stack";
+export { EnvironmentProvider } from "@equinor/mad-components";
