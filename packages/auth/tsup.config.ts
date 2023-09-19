0230a1354fb2d453fe17eefc6eb0ab788267326d
Tormod Aase <toraas@equinor.com>
2023-09-19 12:42:52 +0200

    fix tsup config

--- packages/auth/tsup.config.ts
+++ packages/auth/tsup.config.ts
@@ -8,20 +8,9 @@ export default defineConfig({
     dts: false,
     format: "esm",
     tsconfig: "./tsconfig.json",
-    loader: {
-        ".otf": "copy",
-    },
-    esbuildOptions(options) {
-        options.assetNames = "assets/fonts/[name]";
-    },
     async onSuccess() {
         // eslint-disable-next-line no-console
         console.log("⚙️ Generating typescript declarations..");
-        spawnSync("tsc", [
-            "--project",
-            "tsconfig.json",
-            "--emitDeclarationOnly",
-            "--declaration",
-        ]);
+        spawnSync("tsc", ["--project", "tsconfig.json", "--emitDeclarationOnly", "--declaration"]);
     },
 });
