49490f6e7b21b67c42cd733dcb57b8697c1db4e4
Tormod Aase <toraas@equinor.com>
2023-10-13 12:44:18 +0200

    feat: add scopes prop to login button

---
+++ .changeset/shaggy-berries-join.md
@@ -0,0 +1,5 @@
+---
+"@equinor/mad-auth": patch
+---
+
+added scopes prop to the login button
--- apps/chronicles/screens/LoginScreen.tsx
+++ apps/chronicles/screens/LoginScreen.tsx
@@ -18,6 +18,7 @@ export const LoginScreen = () => {
             <LoginButton
                 redirectUri="msauth.com.equinor.mad.chronicles://auth"
                 clientId="49222fe1-4e0a-4310-9e81-1a2c3eb9b2ed"
+                scopes={["0a429637-3fe1-4452-bd95-c87923ba340b/user_impersonation"]}
                 onAuthenticationSuccessful={() => navigation.navigate("Root")}
                 enableAutomaticAuthentication
             />
