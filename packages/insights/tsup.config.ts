8f5df4de432e2a59eb6005b868fea60c41e8ccc6
Tormod Aase <toraas@equinor.com>
2023-10-26 09:48:17 +0200

    fix: `.d.ts` files are now created using tsup instead of tsc (#249)

--- packages/insights/tsup.config.ts
+++ packages/insights/tsup.config.ts
@@ -1,16 +1,10 @@
-import { spawnSync } from "child_process";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["./src/**/*.ts?(x)", "!./src/types.d.ts", "!./src/__tests__/*"],
     splitting: true,
     clean: true,
-    dts: false,
+    dts: true,
     format: "esm",
     tsconfig: "./tsconfig.json",
-    async onSuccess() {
-        // eslint-disable-next-line no-console
-        console.log("⚙️ Generating typescript declarations..");
-        spawnSync("tsc", ["--project", "tsconfig.json", "--emitDeclarationOnly", "--declaration"]);
-    },
 });
