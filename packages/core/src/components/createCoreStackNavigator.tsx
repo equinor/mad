3ad0470400b15ee788ff87913faf5d07c7a28a77
Jonas-Omdal <jonasomdal@gmail.com>
2023-11-02 13:13:29 +0100

    Test

--- packages/core/src/components/createCoreStackNavigator.tsx
+++ packages/core/src/components/createCoreStackNavigator.tsx
@@ -2,6 +2,7 @@ import { CoreStackParamListBase, MadConfig } from "../types";
 import { createNativeStackNavigator } from "@equinor/mad-navigation";
 import { createMadCoreNavigator } from "../utils/createMadCoreNavigator";
 import { setConfig } from "../store/mad-config";
+import type {} from "@react-navigation/native-stack";
 
 export const createCoreStackNavigator = (config: MadConfig) => {
     setConfig(config);
