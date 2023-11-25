c011bb9bb251d3756f33c510bfdcf59d4afe5fe3
HaakonSvane <haakonsvane@gmail.com>
2023-11-25 10:47:09 +0100

    doc strings

--- packages/skia-draw/src/hooks/useCanvasControl.ts
+++ packages/skia-draw/src/hooks/useCanvasControl.ts
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import { CanvasControlContext } from "../CanvasControlProvider/CanvasControlProvider";
 
+/**
+ * Hook that taps into the CanvasControlContext to provide common getters, setters and canvas control methods.
+ * This hook is meant to be used in conjunction with the CanvasControlProvider.
+ * Use this hook in your custom control panels to communicate with the canvas state.
+ */
 export const useCanvasControl = () => {
     const {
         toolColor,
