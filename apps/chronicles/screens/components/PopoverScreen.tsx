0846f7836d5ca5f591c1a4af2a7d9bee2192897d
HaakonSvane <haakonsvane@gmail.com>
2023-11-25 14:57:46 +0100

    feat: make onClose prop on  component optional

--- apps/chronicles/screens/components/PopoverScreen.tsx
+++ apps/chronicles/screens/components/PopoverScreen.tsx
@@ -10,8 +10,12 @@ import React, { useRef, useState } from "react";
 import { ScrollView, View } from "react-native";
 
 export const PopoverScreen = () => {
-    const buttonRef = useRef<View>(null);
-    const [open, setOpen] = useState(false);
+    const firstButtonRef = useRef<View>(null);
+    const [isFirstPopoverOpen, setIsFirstPopoverOpen] = useState(false);
+
+    const secondButtonRef = useRef<View>(null);
+    const [isSecondPopoverOpen, setIsSecondPopoverOpen] = useState(false);
+
     const style = useStyles(themeStyles);
     return (
         <ScrollView
@@ -19,28 +23,61 @@ export const PopoverScreen = () => {
             contentContainerStyle={style.contentContainer}
         >
             <Typography>
-                {
-                    "The popover component displays modally on top of your content and positions itself \
-                automatically based on constraints around the anchor element so you won't have to \
-                worry about it being positioned badly."
-                }
+                {"The popover component displays modally on top of your content and positions " +
+                    "itself automatically based on constraints around the anchor element so you " +
+                    "won't have to worry about it being positioned badly."}
             </Typography>
-            <Spacer amount="large" />
+            <Spacer />
             <Button
                 title="Trigger popover"
-                ref={buttonRef}
+                ref={firstButtonRef}
                 onPress={() => {
-                    setOpen(!open);
+                    setIsFirstPopoverOpen(!isFirstPopoverOpen);
                 }}
             />
             <Popover
-                open={open}
-                anchorEl={buttonRef}
-                onClose={() => setOpen(false)}
+                open={isFirstPopoverOpen}
+                anchorEl={firstButtonRef}
+                onClose={() => setIsFirstPopoverOpen(false)}
                 placement="top"
             >
                 <Typography>This is a popover</Typography>
             </Popover>
+
+            <Spacer amount="large" />
+            <Typography>
+                {"If you want to have the popover stay open while interacting " +
+                    "with other elements outside of it simply don't provide " +
+                    "an onClose callback. Though this might seem like convenient " +
+                    "functionality, please note that nothing prevents the user " +
+                    "from interacting with the rest of the app while the popover " +
+                    "is open, meaning that they can potentially end up in strange UI states."}
+            </Typography>
+            <Spacer />
+            <Button
+                title="My popover won't stop you from interacting.."
+                ref={secondButtonRef}
+                onPress={() => {
+                    setIsSecondPopoverOpen(!isFirstPopoverOpen);
+                }}
+            />
+            <Popover open={isSecondPopoverOpen} anchorEl={secondButtonRef} placement="top">
+                <Typography>{"But remember to provide a way to close it!"}</Typography>
+                <Spacer amount="small" />
+                <Button
+                    title="click me to close the popover"
+                    iconName="close"
+                    iconPosition="leading"
+                    variant="outlined"
+                    onPress={() => setIsSecondPopoverOpen(false)}
+                />
+            </Popover>
+            <Spacer />
+            <View style={{ width: "100%", flexDirection: "row", justifyContent: "center" }}>
+                <Button variant="outlined" title="...with me..." />
+                <Spacer.Horizontal />
+                <Button variant="outlined" title="...or me" />
+            </View>
         </ScrollView>
     );
 };
