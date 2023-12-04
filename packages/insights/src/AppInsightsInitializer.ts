8c7d54593cc93287a4ef2015ebe0432f9d024a41
Tormod Aase <toraas@equinor.com>
2023-12-04 11:57:39 +0100

    PR feedback

--- packages/insights/src/AppInsightsInitializer.ts
+++ packages/insights/src/AppInsightsInitializer.ts
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import { AppInsightsInitConfig } from "./types";
 import { useAppInsights } from "./useAppInsights";
 
