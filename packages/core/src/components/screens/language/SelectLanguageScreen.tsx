887ed2658a6b4a0f9e1e0ece673253e6ed659c6f
Tormod Aase <toraas@equinor.com>
2023-12-11 14:56:21 +0100

    Tiny fixes

--- packages/core/src/components/screens/language/SelectLanguageScreen.tsx
+++ packages/core/src/components/screens/language/SelectLanguageScreen.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useScreenTitleFromDictionary } from "../../../hooks/useScreenTitleFromDictionary"
 import { useLanguage } from "../../../store/language";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { SelectLanguageCellGroup } from "./components/SelectLanguageCellGroup";
-import { Button, EDSStyleSheet, useStyles } from "@equinor/mad-components";
+import { Button, EDSStyleSheet, Spacer, useStyles } from "@equinor/mad-components";
 import { useDictionary } from "../../../language/useDictionary";
 import { useCoreStackNavigation, useCoreStackRoute } from "../../../hooks";
 import { getNavigationRouteForLanguageSelectScreen } from "../../../utils/getNavigationRouteForSelectLanguageScreen";
@@ -25,12 +25,13 @@ export const SelectLanguageScreen = () => {
         navigation.navigate(route);
     }
 
-    return <View>
+    return <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Spacer />
         <SelectLanguageCellGroup selectedLanguageCodeDraft={selectedLanguageCodeDraft} setSelectedLanguageCodeDraft={setSelectedLanguageCodeDraft}/>
         <View style={styles.submitButtonContainer}>
             <Button title={dictionary.common.submit} onPress={onPressSubmit} />
         </View>
-    </View>
+    </ScrollView>
 }
 
 const themeStyles = EDSStyleSheet.create((theme) => ({submitButtonContainer: {flexDirection: 'column', alignItems: 'flex-end', paddingVertical: theme.spacing.container.paddingVertical, paddingHorizontal: theme.spacing.container.paddingHorizontal}}))
