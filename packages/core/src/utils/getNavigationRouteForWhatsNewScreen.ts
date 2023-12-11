ddd4cf150b005a277e3cf924edbd124002d92cd4
Tormod Aase <toraas@equinor.com>
2023-12-11 15:58:57 +0100

    fix linting

--- packages/core/src/utils/getNavigationRouteForWhatsNewScreen.ts
+++ packages/core/src/utils/getNavigationRouteForWhatsNewScreen.ts
@@ -5,6 +5,8 @@ import { CoreStackParamListBase } from "../types";
 export const getNavigationRouteForWhatsNewScreen = (): keyof CoreStackParamListBase => {
     const isLanguageSelected = getIsLanguageSelected();
     const config = getConfig();
-    if (isLanguageSelected || config.language.skipOnboarding || config.language.supportedLanguages.length < 2) return "Root";
+    if (config.language.supportedLanguages.length < 2) return "Root";
+    if (config.language.skipOnboarding) return "Root";
+    if (isLanguageSelected) return "Root";
     return "SelectLanguageOnboarding"
 }
