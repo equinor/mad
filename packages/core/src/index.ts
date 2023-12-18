df74412d3b5ee2d7afed7bce0931bf319e41756d
Tormod Aase <tormod.aase@bouvet.no>
2023-12-18 12:55:50 +0100

    fix: export authenticateSilently

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./types";
 export * from "./components";
+export { authenticateSilently } from "@equinor/mad-auth";
 export { addTelemetryInitializer, Envelope } from "@equinor/mad-insights";
 export * from "./hooks";
