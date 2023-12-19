664d2b12c33d6541e8baac856fceeab59def4ae4
Tormod Aase <tormod.aase@bouvet.no>
2023-12-19 19:35:00 +0100

    export everything from mad-insight

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./types";
 export * from "./components";
 export { authenticateSilently } from "@equinor/mad-auth";
-export { addTelemetryInitializer, Envelope } from "@equinor/mad-insights";
+export * from "@equinor/mad-insights";
 export * from "./hooks";
 export * from "./store";
