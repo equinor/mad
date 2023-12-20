7c9c6da5d0ac28af4dabd713d91dfbf9079aeacb
Tormod Aase <toraas@equinor.com>
2023-12-20 09:22:35 +0100

    fix: export almost everything from `mad-insight` (#326)

---
+++ .changeset/thirty-deers-sip.md
@@ -0,0 +1,5 @@
+---
+"@equinor/mad-core": patch
+---
+
+export most of `mad-insights`' exports
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,16 @@
 export * from "./types";
 export * from "./components";
 export { authenticateSilently } from "@equinor/mad-auth";
-export { addTelemetryInitializer, Envelope } from "@equinor/mad-insights";
+export {
+    appInsightsHasBeenInitialized,
+    addTelemetryInitializer,
+    Envelope,
+    metricKeys,
+    metricStatus,
+    track,
+    trackCustom,
+    trackLongTerm,
+    trackShortTerm,
+} from "@equinor/mad-insights";
 export * from "./hooks";
 export * from "./store";
