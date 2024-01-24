799ada94d4c62d0fbba182fd4e47eee78c6ff75a
Tormod Aase <tormod.aase@bouvet.no>
2024-01-24 13:54:28 +0100

    make sure tests work, added 2 additional test suites

--- packages/core/__tests__/utils/getNavigationRouteForLoginScreen.spec.ts
+++ packages/core/__tests__/utils/getNavigationRouteForLoginScreen.spec.ts
@@ -1,18 +1,29 @@
 import { getNavigationRouteForLoginScreen } from "../../src/utils/getNavigationRouteForLoginScreen";
 
 describe("getNavigationRouteForLoginScreen", () => {
+    const defaultArguments = {
+        appVersion: "0.0.0",
+        lastDisplayedReleaseNotesVersion: "0.0.0",
+        isDemoModeEnabled: false,
+        supportedLanguages: [],
+        isLanguageSelected: true,
+        skipOnboarding: false,
+    };
     it("should always return 'WhatsNew' when demo mode is set to true", () => {
         const route1 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "1.0.0",
             lastDisplayedReleaseNotesVersion: "1.0.0",
             isDemoModeEnabled: true,
         });
         const route2 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "1.0.0",
             lastDisplayedReleaseNotesVersion: "1.1.0",
             isDemoModeEnabled: true,
         });
         const route3 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "1.1.0",
             lastDisplayedReleaseNotesVersion: "1.0.0",
             isDemoModeEnabled: true,
@@ -29,48 +40,56 @@ describe("getNavigationRouteForLoginScreen", () => {
         // Here is a simplified version
 
         const val1 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.0",
             lastDisplayedReleaseNotesVersion: "0.0.0",
         });
         expect(val1).toBe(null);
 
         const val2 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.0",
             lastDisplayedReleaseNotesVersion: "0.0.10",
         });
         expect(val2).toBe(null);
 
         const val3 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.10",
             lastDisplayedReleaseNotesVersion: "0.0.0",
         });
         expect(val3).toBe("WhatsNew");
 
         const val4 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.10",
             lastDisplayedReleaseNotesVersion: "0.10.0",
         });
         expect(val4).toBe(null);
 
         const val5 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.10.0",
             lastDisplayedReleaseNotesVersion: "0.0.10",
         });
         expect(val5).toBe("WhatsNew");
 
         const val6 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.10.10",
             lastDisplayedReleaseNotesVersion: "10.0.0",
         });
         expect(val6).toBe(null);
 
         const val7 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "10.0.0",
             lastDisplayedReleaseNotesVersion: "0.10.10",
         });
         expect(val7).toBe("WhatsNew");
 
         const val8 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.0",
             lastDisplayedReleaseNotesVersion: "-1",
         });
@@ -79,11 +98,13 @@ describe("getNavigationRouteForLoginScreen", () => {
 
     it("Should always return 'WhatsNew' if lastDisplayedReleaseNotesVersion is null", () => {
         const val1 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "0.0.0",
             lastDisplayedReleaseNotesVersion: null,
         });
         expect(val1).toBe("WhatsNew");
         const val2 = getNavigationRouteForLoginScreen({
+            ...defaultArguments,
             appVersion: "",
             lastDisplayedReleaseNotesVersion: null,
         });
