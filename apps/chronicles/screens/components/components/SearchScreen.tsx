de9a4abbc7049c8f0d29eaf08300806d09036553
Jonas-Omdal <jonasomdal@gmail.com>
2024-01-29 14:00:00 +0100

    fix(components): adjust description of the search component

--- apps/chronicles/screens/components/components/SearchScreen.tsx
+++ apps/chronicles/screens/components/components/SearchScreen.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { ScrollView } from "react-native";
-import { EDSStyleSheet, useStyles, Typography, Spacer, Search } from "@equinor/mad-components";
+import {
+    EDSStyleSheet,
+    useStyles,
+    Typography,
+    Spacer,
+    Search,
+    Button,
+} from "@equinor/mad-components";
 
 export const SearchScreen = () => {
     const styles = useStyles(themedStyles);
@@ -9,15 +16,26 @@ export const SearchScreen = () => {
             contentInsetAdjustmentBehavior="automatic"
             contentContainerStyle={styles.container}
         >
-            <Typography>You can use an Input to add left adornments</Typography>
-            <Typography>Standard search bar</Typography>
+            <Typography>
+                The Search component can be used to enter and submit search queries.
+            </Typography>
+            <Spacer />
+
+            <Typography>Standard Search allows users to input search queries.</Typography>
             <Spacer amount="small" />
-            <Search placeholder="Search"></Search>
+            <Search placeholder="Search here..."></Search>
             <Spacer amount="large" />
 
-            <Typography>Search bar with cancel button</Typography>
+            <Typography>
+                Search with the «cancellable» prop. The cancel button allows users to clear their
+                search and exit the search mode.
+            </Typography>
             <Spacer amount="small" />
-            <Search placeholder="Search" cancellable />
+            <Search placeholder="Search here..." cancellable />
+
+            <Spacer amount="medium" />
+
+            <Button title="test" onPress={() => console.warn("TEST PRESSED")} />
         </ScrollView>
     );
 };
