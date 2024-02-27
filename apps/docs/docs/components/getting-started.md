170c656d6a3068bd3bf5eed12fd0caaceb9bf476
HaakonSvane <haakonsvane@gmail.com>
2024-02-27 08:20:28 +0100

    create base components docs

--- apps/docs/docs/components/getting-started.md
+++
@@ -1,6 +0,0 @@
----
-sidebar_label: Getting started
-description: Getting started with our mobile components!
----
-
-# How to get started
--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,24 +1,27 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <header style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <div style={{ width: "100%", height: 800, backgroundColor: "lightgray" }} />
+        <div style={{ display: "flex", flex: 1, flexDirection: "column", alignItems: "center" }}>
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    flex: 1,
+                    backgroundImage: `url("img/mad-platform.png")`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "contain",
+                    backgroundPosition: "center",
+                }}
+            />
             <h1 style={{ fontWeight: 700 }}>{siteConfig.title}</h1>
-        </header>
+        </div>
     );
 }
 
 export default function Home(): JSX.Element {
-    const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
             noFooter
