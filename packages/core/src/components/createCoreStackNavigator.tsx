971f2da026fa6eeb0138b10c56fe50d38990d559
HaakonSvane <haakonsvane@gmail.com>
2024-03-05 08:18:48 +0100

    add missing devDeps to packages

--- packages/core/src/components/createCoreStackNavigator.tsx
+++ packages/core/src/components/createCoreStackNavigator.tsx
@@ -1,9 +1,9 @@
-import { CoreStackParamListBase, MadConfig } from "../types";
 import { createNativeStackNavigator } from "./navigation";
 import { createMadCoreNavigator } from "../utils/createMadCoreNavigator";
 import { setConfig } from "../store/mad-config";
-import { ParamListBase } from "@react-navigation/native";
 import { initiateAuthenticationClient } from "../utils/initiateAuthenticationClient";
+import type { CoreStackParamListBase, MadConfig } from "../types";
+import type { ParamListBase } from "@react-navigation/native";
 
 export const createCoreStackNavigator = <T extends ParamListBase>(config: MadConfig<T>) => {
     setConfig(config as MadConfig);
