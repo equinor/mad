01ad8cc4aaac7a821ede0a6cd6441441fb3a4da9
havn <havn@equinor.com>
2024-03-08 11:21:46 +0100

    WIP

--- packages/auth/src/Expo-AuthSession/ExpoLoginButton.tsx
+++ packages/auth/src/Expo-AuthSession/ExpoLoginButton.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@equinor/mad-components";
 import * as WebBrowser from "expo-web-browser";
-import { useAuthRequest, useAutoDiscovery } from "expo-auth-session";
-import {LoginButtonProps} from "../components";
+import {
+    useAuthRequest,
+    AccessTokenRequest,
+    exchangeCodeAsync,
+    useAutoDiscovery,
+    ResponseType,
+    fetchUserInfoAsync,
+} from "expo-auth-session";
+import { LoginButtonProps } from "../components";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -25,9 +32,55 @@ export const ExpoLoginButton = ({
             clientId: clientId,
             scopes: scopes,
             redirectUri: redirectUri,
+            responseType: ResponseType.Code,
         },
         discovery,
     );
+
+    useEffect(() => {
+        if (response && response?.type === "error") {
+            onAuthenticationFailed(response.error);
+            return;
+        }
+
+        if (!discovery || response?.type !== "success") return;
+        const code = response.params["code"];
+        if (!code) return;
+
+        const getToken = async () => {
+            console.log("authenticating!");
+            try {
+                const accessToken = new AccessTokenRequest({
+                    code,
+                    clientId,
+                    redirectUri,
+                    scopes,
+                    extraParams: {
+                        code_verifier: request?.codeVerifier ? request.codeVerifier : "",
+                    },
+                });
+
+                const tokenResponse = await exchangeCodeAsync(accessToken, discovery);
+                const userId = await fetchUserInfoAsync(tokenResponse, discovery);
+                onAuthenticationSuccessful(
+                    {
+                        accessToken: tokenResponse.accessToken,
+                        account: {
+                            name: userId["name"] as string,
+                            username: userId["email"] as string,
+                            identifier: userId["identifier"] as string,
+                        },
+                    },
+                    "MANUAL",
+                );
+            } catch (e: any) {
+                console.log("failed!")
+                console.log(e);
+                onAuthenticationFailed(e);
+            }
+        };
+        getToken();
+    }, [response, discovery]);
 
     const onPress = async () => {
         console.log("here we go!");
