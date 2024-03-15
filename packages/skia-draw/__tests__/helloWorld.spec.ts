0b9e8e33394fd1d3959768e68858c3c3cc07ea51
Tormod Aase <tormod.aase@bouvet.no>
2024-03-15 12:22:48 +0100

    chore(test): run tests in lint pr

---
+++ packages/skia-draw/__tests__/helloWorld.spec.ts
@@ -0,0 +1,5 @@
+describe("Hello world", () => {
+    it("is a hello world test to make sure the 'yarn test:all' passes. No other test is implemented yet. When you have started implementing tests in this package, you can delete this", () => {
+        expect(true).toBe(true);
+    });
+});
