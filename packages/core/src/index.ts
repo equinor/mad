abb6d1aad70e1db6a796fbbae75bd72c378e8dc4
Håvard Notland <110901733+HavardNot@users.noreply.github.com>
2024-05-07 12:10:39 +0200

    fix: export issue (#512)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -26,32 +26,35 @@ import {
     ExpoAuthSession,
 } from "@equinor/mad-auth";
 
-const useExpoAuthSession = getConfig().experimental.useExpoAuthSession;
+const hasExpoAuthSession = () => getConfig().experimental.useExpoAuthSession;
 
 /**
  * @JSDocs This export will depend on whether you have opted in to use expo-auth-session. They will function the same, but
  * scopes will not be applied when using expo-auth-session
  */
 export const authenticateSilently = (scopes: string[]) =>
-    useExpoAuthSession ? ExpoAuthSession.authenticateSilently() : msalAuthenticateSilently(scopes);
+    hasExpoAuthSession()
+        ? ExpoAuthSession.authenticateSilently()
+        : msalAuthenticateSilently(scopes);
 /**
  * @JSDocs This export will depend on whether you have opted in to use expo-auth-session. They will function the same, but
  * getAcount() from expo-auth-session is not inherently async
  */
 export const getAccount = async (): Promise<MadAccount | null> =>
-    useExpoAuthSession ? Promise.resolve(ExpoAuthSession.getAccount()) : msalGetAccount();
+    hasExpoAuthSession() ? Promise.resolve(ExpoAuthSession.getAccount()) : msalGetAccount();
 
 /**
  * @JSDocs This export will depend on whether you have opted in to use expo-auth-session. They will function the same, but
  * signOut() from expo-auth-session is not inherently async
  */
-export const signOut = async () => (useExpoAuthSession ? ExpoAuthSession.signOut() : msalSignOut());
+export const signOut = async () =>
+    hasExpoAuthSession() ? ExpoAuthSession.signOut() : msalSignOut();
 
 /**
  * @JSDocs This export will depend on whether you have opted in to use expo-auth-session. They will function the same, but
  * scopes will not be applied when using expo-auth-session
  */
 export const authenticateInteractively = (scopes: string[]) =>
-    useExpoAuthSession
+    hasExpoAuthSession()
         ? ExpoAuthSession.authenticateInteractively()
         : msalAuthenticateInteractively(scopes);
