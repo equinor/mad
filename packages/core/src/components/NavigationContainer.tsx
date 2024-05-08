c2404afd09d51adf81d2dee1f7fb7c6083b2d260
Tormod Aase <toraas@equinor.com>
2024-05-08 12:01:05 +0200

    fix: ref works on `NavigationContainer` (#514)

--- packages/core/src/components/NavigationContainer.tsx
+++ packages/core/src/components/NavigationContainer.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { ForwardedRef } from "react";
 import {
     NavigationContainer as MadNavigationContainer,
     NavigationContainerProps,
 } from "@equinor/mad-navigation";
 import { trackNavigation } from "@equinor/mad-insights";
+import { NavigationContainerRef } from "@react-navigation/native";
 
-export const NavigationContainer = <T extends object>(props: NavigationContainerProps<T>) => (
-    <MadNavigationContainer
+const NavigationContainerInner = <T extends object>(
+    props: NavigationContainerProps<T>,
+    ref?: ForwardedRef<NavigationContainerRef<T>>,
+) => (
+    <MadNavigationContainer<T>
         {...props}
+        ref={ref}
         onRouteChange={(current, prevRoute) => {
             trackNavigation(current);
             props.onRouteChange?.(current, prevRoute);
         }}
     />
 );
+
+export const NavigationContainer = React.forwardRef(
+    NavigationContainerInner,
+) as typeof MadNavigationContainer;
