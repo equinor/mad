9f571847d32a487bed1f42a7e5558aace2ee891f
Håvard Notland <110901733+HavardNot@users.noreply.github.com>
2024-05-21 09:19:23 +0200

    feat: deploy chronicles to radix (#529)
    Added deploy setup for hosting Chronicles Web in Radix:
    https://web-chronicles-test.radix.equinor.com/
    https://web-chronicles-prod.radix.equinor.com/
    
    Currently using tags to push build requests to radix, feel free to add
    your own suggestions and opinions on how we should schedule releases to
    test and production 👍
    
    EDIT: Added release to production in the publish:all script and
    configured Radix to deploy to test from main

--- apps/chronicles/mad.config.ts
+++ apps/chronicles/mad.config.ts
@@ -8,7 +8,7 @@ export const config: MadConfig<RootStackParamList> = {
     navigateToMainRouteFn: navigation => navigation.navigate("Root"),
     appVersion: "1.0.0",
     servicePortalName: "Chronicles",
-    currentEnvironment: "prod",
+    currentEnvironment: "dev",
     serviceNow: "MAD",
     language: {
         supportedLanguages: [
@@ -21,15 +21,21 @@ export const config: MadConfig<RootStackParamList> = {
     authentication: {
         prod: {
             redirectUri: "msauth.com.equinor.mad.chronicles://auth",
-            redirectUriWeb: "http://localhost:8081",
+            redirectUriWeb: "https://web-chronicles-prod.radix.equinor.com/",
             clientId: "49222fe1-4e0a-4310-9e81-1a2c3eb9b2ed",
             scopes: ["0a429637-3fe1-4452-bd95-c87923ba340b/user_impersonation"],
         },
         test: {
             redirectUri: "msauth.com.equinor.mad.chronicles://auth",
-            redirectUriWeb: "http://localhost:8081",
+            redirectUriWeb: "https://web-chronicles-test.radix.equinor.com/",
             clientId: "49222fe1-4e0a-4310-9e81-1a2c3eb9b2ed",
             scopes: ["830a7388-cd89-4e25-a631-bd615bf225a4/user_impersonation"],
+        },
+        dev: {
+            redirectUri: "msauth.com.equinor.mad.chronicles://auth",
+            redirectUriWeb: "http://localhost:8081",
+            clientId: "49222fe1-4e0a-4310-9e81-1a2c3eb9b2ed",
+            scopes: ["0a429637-3fe1-4452-bd95-c87923ba340b/user_impersonation"],
         },
     },
     login: {
