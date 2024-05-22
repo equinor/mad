1b953028bcb2406a31177789633c0b1f40873e22
Tormod Aase <tormod.aase@bouvet.no>
2024-05-22 15:46:05 +0200

    Fix broken links

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -99,7 +99,11 @@ const config: Config = {
                         },
                         {
                             label: "MAD Core",
-                            to: "docs/mad-core/getting-started",
+                            to: "docs/mad-core/introduction",
+                        },
+                        {
+                            label: "MAD Toast",
+                            to: "docs/mad-toast/introduction",
                         },
                     ],
                 },
