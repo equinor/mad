1c1337a373bdbd940bc38d5e31a6f2449e0d7ca9
Fredrik Netteland <fredrik.netteland@gmail.com>
2024-05-26 10:46:00 +0200

    Scanner now works

--- apps/chrosetta/screens/OCRScreen.tsx
+++ apps/chrosetta/screens/OCRScreen.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { OCRCamera } from "@equinor/react-native-mad-tag-scanner";
 
 export const OCRScreen = () => {
-    return <OCRCamera onClose={()=>console.log("close")} onSelectTag={(tag)=>console.log(tag)} />
+    return <OCRCamera onClose={() => console.log("close")} onSelectTag={tag => console.log(tag)} />;
 };
