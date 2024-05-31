e631cf6fa60b5af682819a63a659a607662fb104
Fredrik Netteland <fredrik.netteland@gmail.com>
2024-05-31 15:00:12 +0200

    Update OCR screen in chrosetta

--- apps/chrosetta/screens/OCRScreen.tsx
+++ apps/chrosetta/screens/OCRScreen.tsx
@@ -1,27 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { OCRCamera } from "@equinor/react-native-mad-tag-scanner";
 import { addToast } from "@equinor/mad-core";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useCameraPermission } from "react-native-vision-camera";
+import { Color } from "@shopify/react-native-skia";
 
 export const OCRScreen = () => {
     const navigation = useNavigation();
+    const [showPopoverOnClick, setShowPopoverOnClick] = useState(true);
+    const [boundingBoxColor, setBoundingBoxColor] = useState<Color>("red");
     const { hasPermission, requestPermission } = useCameraPermission();
 
     useFocusEffect(() => void requestPermission());
+
+    const extraButtons = [
+        {
+            name: "format-paint",
+            onPress: () => {
+                setBoundingBoxColor(boundingBoxColor === "red" ? "green" : "red");
+                addToast({
+                    type: "info",
+                    text: `Bounding box color is now ${boundingBoxColor.toString()}`,
+                    duration: 2000,
+                    onPress: hide => hide(),
+                });
+            },
+        },
+        {
+            name: showPopoverOnClick ? "menu" : "cursor-default-click",
+            onPress: () => {
+                setShowPopoverOnClick(!showPopoverOnClick);
+                addToast({
+                    type: "info",
+                    text: `Clicked text dialog is now ${
+                        showPopoverOnClick ? "enabled" : "disabled"
+                    }`,
+                    duration: 2000,
+                    onPress: hide => hide(),
+                });
+            },
+        },
+    ];
+
+    const onSelectTag = (tag: string) =>
+        addToast({
+            type: "success",
+            text: `🎉 "${tag}" was selected 🎉`,
+            duration: 5000,
+            onPress: hide => hide(),
+        });
 
     return (
         <>
             {hasPermission && (
                 <OCRCamera
+                    enableConfirmTextDialog={showPopoverOnClick}
+                    boundingBoxColor={boundingBoxColor}
                     onClose={() => navigation.goBack()}
-                    onSelectTag={tag =>
-                        addToast({
-                            type: "info",
-                            text: `🎉 "${tag}" was selected 🎉`,
-                            duration: 5000,
-                        })
-                    }
+                    onSelectTag={onSelectTag}
+                    buttonConfig={{
+                        showCloseButton: true,
+                        showInfoButton: true,
+                        extraButtons,
+                    }}
                 />
             )}
         </>
