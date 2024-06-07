015480068b35cf6ed714cf525ae3c56102525c19
Fredrik Netteland <fredrik.netteland@gmail.com>
2024-06-07 14:47:59 +0200

    remove chrosetta package
    Moved the chrosetta package to another branch called mad-tag-ocr-testing-branch.
    
    This branch can be used for testing until the ocr package can be included in chronicles.

--- apps/chrosetta/README.md
+++
@@ -1,2 +0,0 @@
-This app exists to test out packages that only run on Rosetta simulators. We use this app in order
-to not mess up simple workflow in Chronicles.
--- apps/chrosetta/settings.ts
+++
@@ -1,8 +0,0 @@
-import appJson from "./app.json";
-import { Platform } from "react-native";
-
-export const getBuildNumber = (): string => {
-    const buildNumber =
-        Platform.OS == "ios" ? appJson.expo.ios.buildNumber : appJson.expo.web.buildNumber;
-    return buildNumber;
-};
