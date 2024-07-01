1e2d6724eda4e4c8faa44010b11c87e5e2e0d452
HaakonSvane <haakonsvane@gmail.com>
2024-07-01 13:28:34 +0200

    install yarn 4

--- apps/chronicles/e2e/jest.config.js
+++ apps/chronicles/e2e/jest.config.js
@@ -5,10 +5,6 @@ module.exports = {
     testMatch: ["<rootDir>/e2e/**/*.test.js"],
     testTimeout: 120000,
     maxWorkers: 1,
-    globalSetup: "detox/runners/jest/globalSetup",
-    globalTeardown: "detox/runners/jest/globalTeardown",
-    reporters: ["detox/runners/jest/reporter"],
-    testEnvironment: "detox/runners/jest/testEnvironment",
     setupFiles: ["<rootDir>/e2e/helpers.js"],
     verbose: true,
 };
