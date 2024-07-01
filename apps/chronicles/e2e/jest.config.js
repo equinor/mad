f8a9b5c7b582445db4c2b5f7d0209e9cd5907552
HaakonSvane <haakonsvane@gmail.com>
2024-07-01 21:55:34 +0200

    add detox config back again

--- apps/chronicles/e2e/jest.config.js
+++ apps/chronicles/e2e/jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
     testTimeout: 120000,
     maxWorkers: 1,
     setupFiles: ["<rootDir>/e2e/helpers.js"],
+    globalSetup: "detox/runners/jest/globalSetup",
+     globalTeardown: "detox/runners/jest/globalTeardown",
+     reporters: ["detox/runners/jest/reporter"],
+     testEnvironment: "detox/runners/jest/testEnvironment",
     verbose: true,
 };
