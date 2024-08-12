1ca3bfe329f6dce18d77f06ff1daaf58f6ef440b
Haakon Hafsahl Svane <36899138+HaakonSvane@users.noreply.github.com>
2024-08-12 14:06:10 +0200

    feat(maintenance-api): switch to fetch client (#602)
    - move to using the fetch API client (was axios)
    - remove old dependencies
    - try fixing the redirect issue where authorization headers are not
    included under redirects
    
    closes #601, closes #600

--- packages/api/maintenance-api/src/lib/generated/index.ts
+++ packages/api/maintenance-api/src/lib/generated/index.ts
@@ -2,6 +2,10 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { ActivityCode } from './models/ActivityCode';
 export type { ActivityCodeGroup } from './models/ActivityCodeGroup';
