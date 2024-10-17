1fc45de79720fdee66d9d3e6fdd1fb2ff1b84080
Jonas-Omdal <jonasomdal@gmail.com>
2024-10-17 12:09:39 +0200

    fix: prevent device and device brand from showing on web

--- packages/core/src/components/screens/create-incident/CreateIncidentScreen.tsx
+++ packages/core/src/components/screens/create-incident/CreateIncidentScreen.tsx
@@ -8,7 +8,7 @@ import {
     Typography,
     useStyles,
 } from "@equinor/mad-components";
-import { View, LayoutAnimation, ScrollView, KeyboardAvoidingView } from "react-native";
+import { View, LayoutAnimation, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { UserInfo } from "./UserInfo";
 import { useAccountOrDemoAccount } from "../../../hooks";
 import * as Device from "expo-device";
@@ -17,15 +17,20 @@ import { useEnvironment, useServiceNow } from "../../../store/mad-config";
 import { createIncident, CreateIncidentResponse } from "./createIncident";
 
 export const CreateIncidentScreen = () => {
-    const styles = useStyles(createIncidentStyles);
-    const account = useAccountOrDemoAccount();
-    const serviceNowConfigurationItem = useServiceNow();
-    const environment = useEnvironment();
     const [error, setError] = useState<string | CreateIncidentResponse | null>(null);
     const [ticketNumber, setTicketNumber] = useState<string | undefined>(undefined);
     const [ticketTitle, setTicketTitle] = useState<string | undefined>(undefined);
     const [ticketDescription, setTicketDescription] = useState<string | undefined>(undefined);
     const [isSending, setIsSending] = useState(false);
+
+    const account = useAccountOrDemoAccount();
+    const serviceNowConfigurationItem = useServiceNow();
+    const environment = useEnvironment();
+    const styles = useStyles(createIncidentStyles);
+
+    const timeZone = Localization.getCalendars()[0]?.timeZone ?? "Unknown TimeZone";
+    const locale = Localization.getLocales()[0]?.languageTag ?? "Unknown Locale";
+
     const onSubmit = () => {
         setIsSending(true);
         const data = {
@@ -65,12 +70,11 @@ export const CreateIncidentScreen = () => {
         description += `Device Brand: ${Device.brand}\n`;
         description += `Device: ${Device.deviceName}\n`;
         description += `Operating System: ${Device.osVersion}\n`;
-        description += `Time Zone: ${Localization.timezone}\n`;
-        description += `Locale: ${Localization.locale}\n`;
+        description += `Time Zone: ${timeZone}\n`;
+        description += `Locale: ${locale}\n`;
 
         return `${description}\n${ticketDescription}`;
     };
-
     return (
         <KeyboardAvoidingView
             behavior="padding"
@@ -88,11 +92,15 @@ export const CreateIncidentScreen = () => {
                         </Typography>
                     </View>
                     <UserInfo infoType={"User"} infoValue={account?.username} />
-                    <UserInfo infoType={"Device Brand"} infoValue={Device.brand} />
-                    <UserInfo infoType={"Device"} infoValue={Device.deviceName} />
+                    {Platform.OS !== "web" && (
+                        <UserInfo infoType={"Device Brand"} infoValue={Device.brand} />
+                    )}
+                    {Platform.OS !== "web" && (
+                        <UserInfo infoType={"Device"} infoValue={Device.deviceName} />
+                    )}
                     <UserInfo infoType={"Operating System"} infoValue={Device.osVersion} />
-                    <UserInfo infoType={"Time Zone"} infoValue={Localization.timezone} />
-                    <UserInfo infoType={"Area"} infoValue={Localization.locale} />
+                    <UserInfo infoType={"Time Zone"} infoValue={timeZone} />
+                    <UserInfo infoType={"Locale"} infoValue={locale} />
                     {ticketNumber && (
                         <View style={[styles.popupBox, styles.popupSuccess]}>
                             <Typography>{`Ticket number: ${ticketNumber}`}</Typography>
