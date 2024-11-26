2a645eca81ec4a5d5cfa683a59f7e8cff24ec94c
Haakon Hafsahl Svane <36899138+HaakonSvane@users.noreply.github.com>
2024-11-26 10:31:59 +0100

    feat(progress)!: alter visual expression of Progress component (#680)
    - switch internal Progress component workings to sub-dir structure and
    context for passing information
    - remove `title` prop on `Progress`
    - adjust description on `Progress.Item` to accept either a string or a
    method that takes in relevant arguments
    - remove internal console error on failed tasks
    - rework the Progress Screen in MAD Chronicles
    - update UI of the Progress and Progress Item components
    
    
    
    closes #679

--- packages/components/src/components/Progress/Progress.tsx
+++ packages/components/src/components/Progress/Progress.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { View } from "react-native";
+import { useStyles } from "../../hooks/useStyles";
+import { useValidChildren } from "../../hooks/useValidChildren";
+import { EDSStyleSheet } from "../../styling";
 import { StrictChildrenReactNode } from "../../utils/types";
-import { Cell } from "../Cell";
-import { ProgressItemProps } from "./ProgressItem";
+import { ProgressItemProps } from "./ProgressItem/ProgressItem";
 
 export type ProgressProps = {
-    /**
-     * Optional title for the progress group. This title can be used to provide a heading or context for the set of progress items contained within.
-     */
-    title?: string;
     /**
      * Children elements of the Progress component, which should be one or more `ProgressItem` components. The `Progress` component acts as a container that groups these items together.
      * This allows for structured display of multiple progress-tracking elements, each representing a distinct task or process.
@@ -17,6 +16,21 @@ export type ProgressProps = {
         | StrictChildrenReactNode<ProgressItemProps>[];
 };
 
-export const Progress = ({ title, children }: ProgressProps) => {
-    return <Cell.Group title={title}>{children}</Cell.Group>;
+export const Progress = ({ children }: ProgressProps) => {
+    const styles = useStyles(tokenStyles);
+    const validChildren = useValidChildren(children);
+    return validChildren.map((child, index) => (
+        <Fragment key={`progress-item-${index}`}>
+            {child}
+            {index < validChildren.length - 1 && <View style={styles.divider} />}
+        </Fragment>
+    ));
 };
+
+const tokenStyles = EDSStyleSheet.create(token => ({
+    divider: {
+        marginHorizontal: token.spacing.container.paddingHorizontal,
+        height: token.geometry.border.borderWidth,
+        backgroundColor: token.colors.border.medium,
+    },
+}));
