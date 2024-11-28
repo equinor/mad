9c0a6aff386b8e91ee5d7720b25fcf0b88efdb3a
Tormod Aase <tormod.aase@bouvet.no>
2024-11-28 16:05:07 +0100

    feat: add stack navigator

--- packages/core/src/components/navigation/navigation-creators.ts
+++ packages/core/src/components/navigation/navigation-creators.ts
@@ -1,8 +1,10 @@
 import {
     createBottomTabNavigatorFactory,
     createNativeStackNavigatorFactory,
+    createStackNavigatorFactory,
 } from "@equinor/mad-navigation";
 import { MadCoreSubHeader } from "./MadCoreSubHeader";
 
 export const createBottomTabNavigator = createBottomTabNavigatorFactory(MadCoreSubHeader);
 export const createNativeStackNavigator = createNativeStackNavigatorFactory(MadCoreSubHeader);
+export const createStackNavigator = createStackNavigatorFactory(MadCoreSubHeader);
