0fb373ee21061a738d85c039963e7a703f02a1cc
srvEq <srv@equinor.com>
2025-11-24 16:54:39 +0530

    fix: update-environment-and-releasenotes-to-support-q87

--- packages/core/src/utils/madCommonUtils.ts
+++ packages/core/src/utils/madCommonUtils.ts
@@ -5,6 +5,7 @@ const scopesRecord: Record<Environment, string[]> = {
     test: ["830a7388-cd89-4e25-a631-bd615bf225a4/user_impersonation"],
     qa: ["29c0f71f-8db5-41b4-b285-dba2b4e765fe/user_impersonation"],
     prod: ["0a429637-3fe1-4452-bd95-c87923ba340b/user_impersonation"],
+    q87: ["29c0f71f-8db5-41b4-b285-dba2b4e765fe/user_impersonation"], // Q87 Shows same release notes as QA
 };
 
 export const getMadCommonScopes = (env: Environment) => scopesRecord[env];
