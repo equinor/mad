fd0806d845037057c101415fb2137c7564ca3f37
Subhayu Bhattacharjee <SUBBH@equinor.com>
2026-09-22 15:34:12 +0530

    fix: 854 menu popover rendered below native modals (#878)
    - Replaced 'Portal' in Root Modal component with 'Native Modal' in order
    to fix the bug where Root Modal components were being rendered below
    Native Modal components when using Native Navigator

--- packages/components/src/components/_internal/RootModal.tsx
+++ packages/components/src/components/_internal/RootModal.tsx
@@ -1,27 +1,41 @@
 import React, { PropsWithChildren } from "react";
-import { Portal } from "../Portal";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Modal, Pressable, StyleSheet, View } from "react-native";
 
 type RootModalProps = {
     /**
      * Callback method invoked when the user presses outside the child content.
      */
     onBackdropPress?: () => void;
+    /**
+     * Added a visible prop so the Modal knows when to mount
+     */
+    visible?: boolean;
 };
 
-export const RootModal = ({ onBackdropPress, children }: PropsWithChildren<RootModalProps>) => (
-    <Portal name="root">
+export const RootModal = ({
+    onBackdropPress,
+    visible = true,
+    children
+}: PropsWithChildren<RootModalProps>) => (
+    <Modal
+        visible={visible}
+        transparent={true}
+        animationType="none"
+        onRequestClose={onBackdropPress}
+    >
         {onBackdropPress ? (
             <Pressable
                 onPress={onBackdropPress}
-                style={{ ...StyleSheet.absoluteFillObject, zIndex: 1 }}
+                style={StyleSheet.absoluteFillObject}
             >
-                {children}
+                <Pressable style={StyleSheet.absoluteFillObject} pointerEvents="box-none">
+                    {children}
+                </Pressable>
             </Pressable>
         ) : (
-            <View pointerEvents="box-none" style={{ ...StyleSheet.absoluteFillObject, zIndex: 1 }}>
+            <View pointerEvents="box-none" style={StyleSheet.absoluteFillObject}>
                 {children}
             </View>
         )}
-    </Portal>
+    </Modal>
 );
--- packages/core/src/components/service-message/hooks/useFetchServiceMessage.ts
+++ packages/core/src/components/service-message/hooks/useFetchServiceMessage.ts
@@ -10,7 +10,7 @@ export const useFetchServiceMessage = () => {
     const [isExpanded, setIsExpanded] = useState(false);
     const [isError, setIsError] = useState(false);
 
-    const lastInterval = useRef<number | undefined>(undefined);
+    const lastInterval = useRef<NodeJS.Timeout | undefined>(undefined);
 
     useEffect(() => {
         function fetchServiceMessage() {
